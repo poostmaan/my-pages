00eb01070747e04bca03650545dd41442dd49066
Louis <poostmaan@gmail.com>
2023-07-27 23:08:04 -0400

    signIn procedure and sytles

---
+++ public/js/apis/authApis.js
@@ -0,0 +1,33 @@
+async function signin(user) { 
+    
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    console.log(user);
+    const userRaw = JSON.stringify(user);
+
+    if(Object.entries(user).length == 0) throw new Error('User is not defined')
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: userRaw,
+        redirect: 'follow'
+    };
+
+    try {
+        const userFetch = await fetch(`http://localhost:58513/users/signin`, requestOptions)
+
+        const userData = await userFetch.json();
+
+        if(!userData.ok) {
+            mostrarErroresComoAlertasConRetraso(userData.error);
+            return;
+        }
+
+        delete userData.ok;
+        Storage.setData('auth', userData);
+        location.href = '/album.html'
+    } catch (error) {
+        throw new Error(error);
+    }
+}
